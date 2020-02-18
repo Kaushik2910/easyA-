fa55c8f20d22c0b78940d3ee295e774e08eb21e1
Sadiq Albinalshaikh <sadiqg@hotmail.com>
2020-02-18 14:47:17 -0500

    Added: 1- Fixed and tested authentication for sign up and log in. 2- Imported auth.py to views.py then deleted it. 3- Modified the forms in signup.html and login.html for authentication purposes. 4- Used Firestore database instead of the realtime database from Firebase.
    Missing:
    1- After-authentication actions are still to be implemeneted:
    	-Page redirections
    	-User sessions
    	-etc
    2- User input limitations to only accept "@purdue.edu" emails

--- src/easyA/db.py
+++ src/easyA/db.py
@@ -1,6 +1,11 @@
 import pyrebase
+from google.cloud import firestore
+import os
 
 def init_db():
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str(os.path.abspath('easyA/instance/dbauth.json'))
+    firestore_database = firestore.Client()
+
     config = {
         "apiKey": "AIzaSyCKndXyjPiRqsEWRveZNfSI_-wAC7ZU3EI",
         "authDomain": "easya-b1ec1.firebaseapp.com",
@@ -9,7 +14,7 @@ def init_db():
         "storageBucket": "easya-b1ec1.appspot.com",
     }
 
-    firebase = pyrebase.initialize_app(config)
-    database = firebase.database()
+    firebase_wrapper = pyrebase.initialize_app(config)
+    realtime_database = firebase_wrapper.database()
 
-    return database, firebase
+    return firestore_database, realtime_database, firebase_wrapper
--- src/easyA/views.py
+++ src/easyA/views.py
@@ -1,16 +1,95 @@
 
 from easyA import app
-from flask import render_template
+from easyA import db
 
+from flask import render_template, request
+import google.cloud
+import requests
+import json
+
+firestore_database, realtime_database, firebase_wrapper = db.init_db()
+auth = firebase_wrapper.auth()
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        do_login()
     return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
+    if request.method == 'POST':      
+        do_signup()
     return render_template('signup.html')
+
+#Login function
+def do_login():
+    email = request.form['u_email']
+    password = request.form['u_password']
+
+    try:
+        auth.sign_in_with_email_and_password(email, password)
+        print("User logged in successfully")
+        return
+    except requests.exceptions.HTTPError as e:
+        #Create a dictionary from the error
+        e_dict = json.loads(e.strerror)
+
+        #Check if a credentials error occured
+        if e_dict["error"]["message"] == "INVALID_PASSWORD" or e_dict["error"]["message"] == "EMAIL_NOT_FOUND":
+            print("Incorrect credentials!")
+            return
+        else:
+            #Print error code and message
+            print("HTTPError Code {}: {}".format(e_dict["error"]["code"], e_dict["error"]["message"]))
+            return
+
+#Sign up function
+def do_signup():
+    email = request.form['u_email']
+    password = request.form['u_password']
+
+    email_parts = email.split('@', 2)
+
+    #Check user input for "@purdue.edu"
+    if email_parts[1].casefold() != "@purdue.edu":
+        print("Unsupported email")
+        return
+
+    #Check if user's career ID exists in the database
+    career_id = email_parts[0]
+    user_ref = firestore_database.collection('users').document(career_id)
+
+    if user_ref.get().exists:
+        print("User {} already exists!".format(user_ref.get().to_dict()))
+        return
+    else:
+        try:
+            #Create user
+            auth.create_user_with_email_and_password(email, password)
+
+            #Record the user in the database
+            data = {
+                "email": email,
+            }
+            firestore_database.collection('users').document(career_id).set(data)
+
+            #Log in
+            do_login()
+
+            print('Signup and Login Successful')
+            return
+        except requests.exceptions.HTTPError as e:
+            #Create a dictionary from the error
+            e_dict = json.loads(e.strerror)
+
+            #Print error code and message
+            print("HTTPError Code {}: {}".format(e_dict["error"]["code"], e_dict["error"]["message"]))
+            return
+        except Exception as e:
+            print("Authentication or Database FAILURE - {}".format(e))
+            return
