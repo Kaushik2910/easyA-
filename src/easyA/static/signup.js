ddaf4d876d0a24c71d94264010c1399373f9bde1
lhandalb <lhandalb@github.com>
2020-02-24 23:12:59 -0500

    uodated and debugged signup verification

--- src/easyA/static/signup.js
+++ src/easyA/static/signup.js
@@ -8,11 +8,14 @@ function emailCheck(){
   if (email.match(regex) == null) {
     if (email != "") {
       emailHelp.textContent = "You can only sign up with a Purdue email";
-      form.u_password.classList.add("invalid_field");
+    } else {
+      emailHelp.textContent = "Please enter your Purdue email";
     }
+    form.u_email.classList.add("invalid_field");
     return false;
   }
   emailHelp.textContent = "";
+  form.u_email.classList.remove("invalid_field");
   return true;
 }
 
@@ -23,31 +26,34 @@ function passwordCheck() {
 
   var pwdHelp = document.getElementById("pwdHelp");
 
+  form.u_password.classList.remove("invalid_field");
+  form.u_password2.classList.remove("invalid_field");
+
   if (password1 == '') {
     pwdHelp.textContent = "Please enter a password";
+    form.u_password.classList.add("invalid_field");
     return false;
   } else if (password1.length < 6) {
     pwdHelp.textContent = "Your password is too short";
+    form.u_password.classList.add("invalid_field");
     return false;
   } else if (password2 == '') {
     pwdHelp.textContent = "Please confirm your password";
+    form.u_password2.classList.add("invalid_field");
     return false;
   } else if (password1 != password2) {
     pwdHelp.textContent = "Password did not match: Please try again...";
+    form.u_password1.classList.add("invalid_field");
+    form.u_password2.classList.add("invalid_field");
     return false;
   }else{
     pwdHelp.textContent = "";
     return true;
   }
 }
-var inputs = document.getElementsByTagName("input");
 
-for (var i = 0; i < inputs.length; i++) {
-  inputs[i].addEventListener("change", function(){
-    if (emailCheck() && passwordCheck()) {
-      document.getElementById("signup_btn").disabled = false;
-    } else {
-      document.getElementById("signup_btn").disabled = true;
-    }
-  });
+function validateForm(){
+  var email = emailCheck()
+  var pwd = passwordCheck()
+  return email && pwd;
 }
